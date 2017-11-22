09ce4da38cdb52d15c3d944f6b7e7b011f6c51da
Matt Glaman <nmd.matt@gmail.com>
2017-11-22 05:09:35 -0600

    Remove debugging line which prints active port.

--- src/DriverFactory.php
+++ src/DriverFactory.php
@@ -45,7 +45,6 @@ class DriverFactory {
     $capabilities = DesiredCapabilities::phantomjs();
     $capabilities->setCapability('phantomjs.page.settings.userAgent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0');
     $port = self::getAvailablePort();
-    print PHP_EOL . 'Listening on ' . $port . PHP_EOL;
     $service = new DriverService(
       '/usr/local/bin/phantomjs',
       $port,
